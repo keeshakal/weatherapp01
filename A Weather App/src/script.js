b82bdfe3f07d354de72032511bb7e93c75e0c96d
Keesha Kallasur <keeshakallasur@gmail.com>
2024-10-02 13:28:34 +0200

    Updated script.js

---
+++ A Weather App/src/script.js
@@ -0,0 +1,45 @@
+const apiKey ="fe1483f743b581b5520a1b725af03a49";
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+consl url = (city)=> 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe1483f743b581b5520a1b725af03a49`;
+
+async function getWeatherByLocation(city){
+
+    const resp =await fetch(url(city),{
+        origin: "cros"
+    })
+    const respData = await resp.json();
+
+    addWeatherToPage(respData);
+}
+
+    function addWeatherToPage(data){
+        const temp = Ktoc(data.main.temp);
+
+        const weather = document.createElement('div')
+        weather.classList.add('weather');
+
+        weather.innderHTML = `
+        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+        <small>${data.weather[0].main}</small>`;
+
+        main.innerHTML="";
+        main.appendChild(weather);
+    };
+
+    function Ktoc(K) {
+        return Math.floor(K - 273.15);
+    }
+
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+
+        const city = search.value;
+
+        if(city){
+            getWeatherByLocation(city)
+        }
+    });
