3da9f8a29bee6c299b452a28a6321c41da56bde5
Keesha Kallasur <keeshakallasur@gmail.com>
2024-10-04 14:32:08 +0200

    Updated css and js

--- A Weather App/src/script.js
+++ A Weather App/src/script.js
@@ -1,41 +1,119 @@
-const apiKey = "fe1483f743b581b5520a1b725af03a49";
-
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-
-const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-async function getWeatherByLocation(city) {
-    const resp = await fetch(url(city));
-    const respData = await resp.json();
-    addWeatherToPage(respData);
-}
-
-function addWeatherToPage(data) {
-    const temp = Ktoc(data.main.temp);
-
-    const weather = document.createElement('div');
-    weather.classList.add('weather');
-
-    weather.innerHTML = `
-        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-        <small>${data.weather[0].main}</small>`;
-
-    main.innerHTML = "";
-    main.appendChild(weather);
-}
-
-function Ktoc(K) {
-    return Math.floor(K - 273.15);
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const city = search.value;
-
-    if (city) {
-        getWeatherByLocation(city);
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#current-temp");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let currentDayElement = document.querySelector("#day");
+    let currentTimeElement = document.querySelector("#time");
+    let currentDate = new Date();
+    let iconElement = document.querySelector("#icon");
+  
+    cityElement.innerHTML = response.data.city;
+    currentDayElement.innerHTML = formatDay(currentDate);
+    currentTimeElement.innerHTML = formatTime(currentDate);
+    descriptionElement.innerHTML = titleCase(response.data.condition.description);
+    temperatureElement.innerHTML = Math.round(temperature);
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    iconElement.innerHTML =
+      iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  
+    getForecast(response.data.city);
+  }
+  
+  function formatTime(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
     }
-});
+  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    return `${hours}:${minutes}`;
+  }
+  
+  function formatDay(date) {
+    let day = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let formattedDay = days[day];
+    return `${formattedDay}`;
+  }
+  
+  function formatForecastDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+  
+    return days[date.getDay()];
+  }
+  
+  const apiKey = "fe1483f743b581b5520a1b725af03a49";
+  
+  const main = document.getElementById('main');
+  const form = document.getElementById('form');
+  const search = document.getElementById('search');
+    
+  const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe1483f743b581b5520a1b725af03a49`; 
+  
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    searchCity(searchInput.value);
+  }
+  
+  function getForecast(city) {
+    let apiKey = `fe1483f743b581b5520a1b725af03a49`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    axios(apiUrl).then(displayForecast);
+  }
+  
+  function displayForecast(response) {
+    let forecastHtml = " ";
+  
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHtml =
+          forecastHtml +
+          `
+          <div class="forecast-column">
+              <div class="forecast-day">${formatForecastDay(day.time)}</div>
+              <img src = "${day.condition.icon_url}" class="forecast-icon" />
+  
+              <div class="forecast-temp" id="high-forecast"><strong>${Math.round(
+                day.temperature.maximum
+              )}°C</strong></div>
+              <div class="forecast-temp" id="low-forecast">${Math.round(
+                day.temperature.minimum
+              )}°C</div>
+          </div>
+        `;
+      }
+    });
+  
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  function titleCase(str) {
+    return str
+      .toLowerCase()
+      .replace(/(^|\s)\S/g, (firstLetter) => firstLetter.toUpperCase());
+  }
+  
+  searchCity("Johannesburg");
